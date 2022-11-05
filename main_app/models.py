c467349774f1ef5acd6bff4b1a1f8e8c359e2471
root <root@Skinwalker.localdomain>
2022-11-05 18:43:29 -0400

    add index

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Car(models.Model):
+  make = models.CharField(max_length=100)
+  model = models.CharField(max_length=100)
+  year = models.IntegerField()
+  quote = models.TextField(max_length=250)
+
+  def __str__(self):
+    return self.model
--- main_app/views.py
+++ main_app/views.py
@@ -1,29 +1,13 @@
 from django.shortcuts import render
-
-# Add the following import
-from django.http import HttpResponse
-
-# Add the Cat class & list and view function below the imports
-class Car:  # Note that parens are optional if not inheriting from another class
-  def __init__(self, make, model, quote, year):
-    self.make = make
-    self.model = model
-    self.quote = quote
-    self.year = year
-
-cars = [
-  Car('Vector', 'WX8 Hypercar', 'The Car of the Future.', 2007),
-  Car('Rolls-Royce', 'Phantom', '0-60 in 5.5 seconds.', 2022),
-  Car('Cadillac', 'CT-5', 'Take the next step.', 2023),
-  Car('Nissan', '350Z', 'Part of a long family tree', 2006)
-]
+from .models import Car
 
 # Define the home view
 def home(request):
-  return HttpResponse('<h1>Car Time</h1>')
+  return render(request, 'home.html')
 
 def about(request):
   return render(request, 'about.html')
 
 def cars_index(request):
-  return render(request, 'cars/index.html', {'cars': cars})
+  cars = Car.objects.all()
+  return render(request, 'cars/index.html', { 'cars': cars })
